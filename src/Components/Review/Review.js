25a928d2bba2af455d280b67aaac02b8d6a4c22d
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-04-04 03:37:54 +0600

    update review json file and solve reviews all problem

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Review.css'
 
 const Review = ({review}) => {
-    const {name, img, ratings} = review;
+    const {name, img, fedback, ratings} = review;
     return (
             <div className='review-details'>
                 <div>   
@@ -10,8 +10,8 @@ const Review = ({review}) => {
                 </div>
                 <div>
                     <h2> {name}</h2>
-                    <p>{}</p>
-                    <p><small>{ratings} Star</small></p>
+                    <p><strong>Feedback:</strong> {fedback}</p>
+                    <p><strong>Ratings:</strong> <small>{ratings} Star</small></p>
                 </div>
             </div>
     );
