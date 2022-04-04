7e06f8feb7a16ecd28811fec20cd3d697e77e673
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-04-04 06:01:00 +0600

    add rechart in deshboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import MyAreaChart from '../MyAreaChart/MyAreaChart';
+import MyLineChart from '../MyLineChart/MyLineChart';
 
 const Dashboard = () => {
     return (
         <div>
-            <h2>this is dashboard</h2>
+            <div>
+                <h2>this is LineChart</h2>
+                <MyLineChart></MyLineChart>
+            </div>
+            <br />
+            <div>
+                <h2>This is Area Chart</h2>
+                <MyAreaChart></MyAreaChart>
+            </div>
         </div>
     );
 };
